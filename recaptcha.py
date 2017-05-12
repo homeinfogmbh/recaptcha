ae294f46fc0386bdc87761f90b82e1bbe0cfb91a
Richard Neumann <r.neumann@homeinfo.de>
2017-05-12 10:33:20 +0200

    Simplified implementation

--- recaptcha.py
+++ recaptcha.py
@@ -13,49 +13,25 @@ class ReCaptcha(LoggingClass):
 
     VERIFICATION_URL = 'https://www.google.com/recaptcha/api/siteverify'
 
-    def __init__(self, secret, response, remote_ip=None, logger=None):
+    def __init__(self, secret, logger=None):
         """Sets basic reCAPTCHA data"""
         super().__init__(logger=logger)
         self.secret = secret
-        self.response = response
-        self.remote_ip = remote_ip
-        self.reply = None
 
-    def __bool__(self):
+    def validate(self, response, remote_ip=None):
         """Verifies reCAPTCHA data"""
-        self.query()
-        return True if self.verify() else False
+        params = {'secret': self.secret, 'response': response}
 
-    @property
-    def _params(self):
-        """Returns the parameters dictionary for requests"""
-        params = {
-            'secret': self.secret,
-            'response': self.response}
+        if remote_ip is not None:
+            params['remoteip'] = remote_ip
 
-        if self.remote_ip is not None:
-            params['remoteip'] = self.remote_ip
+        reply = post(self.VERIFICATION_URL, params=params)
 
-        return params
-
-    @property
-    def dict(self):
-        """Returns the response dictionary"""
         try:
-            return loads(self.reply.text)
-        except TypeError:
-            self.logger.error('No response available yet')
+            dictionary = loads(reply.text)
         except ValueError:
             self.logger.error('Invalid reCAPTCHA response: {}'.format(
-                self.reply.text))
-
-        return {}
-
-    def query(self, force=False):
-        """Calls the web API"""
-        if self.reply is None or force:
-            self.reply = post(self.VERIFICATION_URL, params=self._params)
-
-    def verify(self):
-        """Verifies reCAPTCHA data"""
-        return self.dict.get('success', False)
+                reply.text))
+            return False
+        else:
+            return dictionary.get('success', False)
