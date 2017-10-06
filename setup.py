b1a8abf899a1cebe7f50947d8ea80d28c44b5b14
Richard Neumann <r.neumann@homeinfo.de>
2017-10-06 16:56:28 +0200

    Updated setup data.

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     name='recaptcha',
     version='latest',
     author='HOMEINFO - Digitale Informationssysteme GmbH',
-    author_email='info@homeinfo.de',
+    author_email='<info at homeinfo dot de>',
     maintainer='Richard Neumann',
-    maintainer_email='r.neumann@homeinfo.de',
+    maintainer_email='<r dot neumann at homeinfo priod de>',
     py_modules=['recaptcha'],
     license='GPLv3',
     description='A library for validating CAPTCHAs.')
