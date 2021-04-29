de7eb824254f217a3996b215097c8540600b72b0
Richard Neumann <mail@richard-neumann.de>
2021-04-29 11:50:11 +0200

    Migrate to setuptools

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 #! /usr/bin/env python3
+"""Installation script."""
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='recaptcha',
-    version='latest',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
     author_email='<info at homeinfo dot de>',
     maintainer='Richard Neumann',
     maintainer_email='<r dot neumann at homeinfo priod de>',
     py_modules=['recaptcha'],
     license='GPLv3',
-    description='A library for validating CAPTCHAs.')
+    description='A library for validating CAPTCHAs.'
+)
