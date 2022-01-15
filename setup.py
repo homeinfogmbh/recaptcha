f2cb6109e8fe9cfc741d3fe078c8d1d59847a468
Richard Neumann <mail@richard-neumann.de>
2022-01-15 13:05:15 +0100

    Update author info

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setup(
     },
     setup_requires=['setuptools_scm'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
-    author_email='<info at homeinfo dot de>',
+    author_email='<info@homeinfo.de>',
     maintainer='Richard Neumann',
-    maintainer_email='<r dot neumann at homeinfo priod de>',
+    maintainer_email='<r.neumann@homeinfo.de>',
     py_modules=['recaptcha'],
     license='GPLv3',
     description='A library for validating CAPTCHAs.'
