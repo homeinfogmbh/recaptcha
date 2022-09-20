5a7f2fbd5e5d06219c239ca52c5fda54efac336c
Richard Neumann <r.neumann@homeinfo.de>
2022-09-20 13:09:18 +0200

    Move exported functions up

--- recaptcha.py
+++ recaptcha.py
@@ -8,7 +8,7 @@ from urllib.parse import urlencode, urlunparse
 from urllib.request import urlopen
 
 
-__all__ = ['VerificationError', 'verify', 'recaptcha']
+__all__ = ['VerificationError', 'recaptcha', 'verify']
 
 
 VERIFICATION_URL = ('https', 'www.google.com', '/recaptcha/api/siteverify')
@@ -25,22 +25,29 @@ class VerificationError(Exception):
         self.json = json
 
 
-def get_url(params: dict) -> str:
-    """Returns a URL with the given parameters."""
+def recaptcha(
+        config: Config,
+        response_getter: Callable[[], str],
+        ip_getter: Optional[Callable[[], str]] = None
+) -> Callable:
+    """Decorator to run a function with previous recaptcha
+    check as defined in the provided configuration.
+    """
 
-    return urlunparse((*VERIFICATION_URL, '', urlencode(params), ''))
+    def decorator(function: Callable[..., Any]) -> Callable[..., Any]:
+        @wraps(function)
+        def wrapper(*args, **kwargs) -> Any:
+            configuration = config() if callable(config) else config
+            params = get_params(configuration, response_getter, ip_getter)
 
+            if verify(*params):
+                return function(*args, **kwargs)
 
-def check_result(json: dict, *, fail_silently: bool = False) -> bool:
-    """Checks the verification result."""
+            raise Exception('Nasty bug in recaptcha.py. Please report.')
 
-    if json.get('success', False):
-        return True
+        return wrapper
 
-    if fail_silently:
-        return False
-
-    raise VerificationError(json)
+    return decorator
 
 
 def verify(
@@ -59,6 +66,24 @@ def verify(
 
     with urlopen(get_url(params)) as http_request:
         return check_result(load(http_request), fail_silently=fail_silently)
+
+
+def get_url(params: dict) -> str:
+    """Returns a URL with the given parameters."""
+
+    return urlunparse((*VERIFICATION_URL, '', urlencode(params), ''))
+
+
+def check_result(json: dict, *, fail_silently: bool = False) -> bool:
+    """Checks the verification result."""
+
+    if json.get('success', False):
+        return True
+
+    if fail_silently:
+        return False
+
+    raise VerificationError(json)
 
 
 def get_bool(config: ConfigType, key: str, fallback: bool = False) -> bool:
@@ -85,28 +110,3 @@ def get_params(
             raise ValueError('IP check requested, but no ip_getter provided.')
 
         yield ip_getter()
-
-
-def recaptcha(
-        config: Config,
-        response_getter: Callable[[], str],
-        ip_getter: Optional[Callable[[], str]] = None
-) -> Callable:
-    """Decorator to run a function with previous recaptcha
-    check as defined in the provided configuration.
-    """
-
-    def decorator(function: Callable[..., Any]) -> Callable[..., Any]:
-        @wraps(function)
-        def wrapper(*args, **kwargs) -> Any:
-            configuration = config() if callable(config) else config
-            params = get_params(configuration, response_getter, ip_getter)
-
-            if verify(*params):
-                return function(*args, **kwargs)
-
-            raise Exception('Nasty bug in recaptcha.py. Please report.')
-
-        return wrapper
-
-    return decorator
