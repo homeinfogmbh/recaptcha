28791565b642c59e4b3b1348f174548a1aaf9ee5
Richard Neumann <r.neumann@homeinfo.de>
2023-07-25 22:46:20 +0200

    Black

--- recaptcha.py
+++ recaptcha.py
@@ -8,10 +8,10 @@ from urllib.parse import urlencode, urlunparse
 from urllib.request import urlopen
 
 
-__all__ = ['VerificationError', 'recaptcha', 'verify']
+__all__ = ["VerificationError", "recaptcha", "verify"]
 
 
-VERIFICATION_URL = ('https', 'www.google.com', '/recaptcha/api/siteverify')
+VERIFICATION_URL = ("https", "www.google.com", "/recaptcha/api/siteverify")
 ConfigType = Union[SectionProxy, dict]
 Config = Union[ConfigType, Callable[[], ConfigType]]
 
@@ -21,14 +21,14 @@ class VerificationError(Exception):
 
     def __init__(self, json: dict):
         """Sets the response object."""
-        super().__init__(*json.get('error-codes', []))
+        super().__init__(*json.get("error-codes", []))
         self.json = json
 
 
 def recaptcha(
-        config: Config,
-        response_getter: Callable[[], str],
-        ip_getter: Optional[Callable[[], str]] = None
+    config: Config,
+    response_getter: Callable[[], str],
+    ip_getter: Optional[Callable[[], str]] = None,
 ) -> Callable:
     """Decorator to run a function with previous recaptcha
     check as defined in the provided configuration.
@@ -43,7 +43,7 @@ def recaptcha(
             if verify(*params):
                 return function(*args, **kwargs)
 
-            raise Exception('Nasty bug in recaptcha.py. Please report.')
+            raise Exception("Nasty bug in recaptcha.py. Please report.")
 
         return wrapper
 
@@ -51,18 +51,18 @@ def recaptcha(
 
 
 def verify(
-        secret: str,
-        response: str,
-        remote_ip: Optional[str] = None,
-        *,
-        fail_silently: bool = False
+    secret: str,
+    response: str,
+    remote_ip: Optional[str] = None,
+    *,
+    fail_silently: bool = False
 ) -> bool:
     """Verifies reCAPTCHA data."""
 
-    params = {'secret': secret, 'response': response}
+    params = {"secret": secret, "response": response}
 
     if remote_ip is not None:
-        params['remoteip'] = remote_ip
+        params["remoteip"] = remote_ip
 
     with urlopen(get_url(params)) as http_request:
         return check_result(load(http_request), fail_silently=fail_silently)
@@ -71,13 +71,13 @@ def verify(
 def get_url(params: dict) -> str:
     """Returns a URL with the given parameters."""
 
-    return urlunparse((*VERIFICATION_URL, '', urlencode(params), ''))
+    return urlunparse((*VERIFICATION_URL, "", urlencode(params), ""))
 
 
 def check_result(json: dict, *, fail_silently: bool = False) -> bool:
     """Checks the verification result."""
 
-    if json.get('success', False):
+    if json.get("success", False):
         return True
 
     if fail_silently:
@@ -96,17 +96,17 @@ def get_bool(config: ConfigType, key: str, fallback: bool = False) -> bool:
 
 
 def get_params(
-        config: ConfigType,
-        response_getter: Callable[[], str],
-        ip_getter: Optional[Callable[[], str]]
+    config: ConfigType,
+    response_getter: Callable[[], str],
+    ip_getter: Optional[Callable[[], str]],
 ) -> Iterator[str]:
     """Returns the verification parameters."""
 
-    yield config.get('secret')
+    yield config.get("secret")
     yield response_getter()
 
-    if get_bool(config, 'check_ip', fallback=False):
+    if get_bool(config, "check_ip", fallback=False):
         if ip_getter is None:
-            raise ValueError('IP check requested, but no ip_getter provided.')
+            raise ValueError("IP check requested, but no ip_getter provided.")
 
         yield ip_getter()
--- setup.py
+++ setup.py
@@ -4,16 +4,14 @@
 from setuptools import setup
 
 setup(
-    name='recaptcha',
-    use_scm_version={
-        "local_scheme": "node-and-timestamp"
-    },
-    setup_requires=['setuptools_scm'],
-    author='HOMEINFO - Digitale Informationssysteme GmbH',
-    author_email='<info@homeinfo.de>',
-    maintainer='Richard Neumann',
-    maintainer_email='<r.neumann@homeinfo.de>',
-    py_modules=['recaptcha'],
-    license='GPLv3',
-    description='A library for validating CAPTCHAs.'
+    name="recaptcha",
+    use_scm_version={"local_scheme": "node-and-timestamp"},
+    setup_requires=["setuptools_scm"],
+    author="HOMEINFO - Digitale Informationssysteme GmbH",
+    author_email="<info@homeinfo.de>",
+    maintainer="Richard Neumann",
+    maintainer_email="<r.neumann@homeinfo.de>",
+    py_modules=["recaptcha"],
+    license="GPLv3",
+    description="A library for validating CAPTCHAs.",
 )
